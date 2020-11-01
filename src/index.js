4f44bbd4faf6ccf50d11f6e33e88031316a8bd05
Eliška Bartošová <bartosova.eli@gmail.com>
2020-11-01 11:26:48 +0100

    skrývání navigace na mobilu

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector('#nav-btn');
+const nav = document.querySelector('#nav');
+const navItem = document.querySelectorAll('#navItem');
+
+navBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+navItem.forEach((navItem) => {
+  navItem.addEventListener('click', () => {
+    nav.classList.toggle('nav-closed');
+  });
+});
